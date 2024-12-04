cc695c45686d17a8a4f8eb64644aff2e88ffa4bc
royisten <nemihidomine@gmail.com>
2024-12-04 16:01:46 +0530

    Day 2/5 // 45%

--- WEEK_1_TASK/QuizGenerator/Question.java
+++ WEEK_1_TASK/QuizGenerator/Question.java
@@ -7,17 +7,40 @@ public class Question implements Serializable {
     private String[] options; // multiple options displayed /1-4
     private int correctOption; // 1-4
 
+    //constructor to create a set of question and answer with option as shown above,
+    //whenever an intance of "Question.java" is called
+    public Question(String questionText ,String[] options, int correctOption ){
+        this.questionText=questionText;
+        this.options=options;
+        this.correctOption=correctOption;
+    }
+    
+    //? Display Question and Options (along with the correct option) to user
+    public void displayQuestion(){
+        System.out.println(questionText);
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i+1)+". "+options[i]);
+        }
+    }
+
+    //? Checks if the user's answer is correct
+    public boolean isCorrect(int userAnswer) {
+        return userAnswer == correctOption;
+    }
+
 }
+
 /*
- * this class creates a single set of a question (plain text) ,
- * it options
+ * This class creates a single set of a question (plain text) ,
+ * and it options
  * (4 options with a right answer)
  * and a the correct anwser no# (like if the correct anwser is option 2 the
  * correctoption = 2)
  * 
- * all in a single "object" of Question and keep on increasing in sets** of
+ * All in a single "object" of Question and keep on increasing in sets** of
  * question if added to a list
  *  
- * this questions can either be displayed or can be created by the user
+ * This questions** can either be displayed (if already created)
+ * or can be created (if empty) by the user
  * 
  */
--- WEEK_1_TASK/QuizGenerator/Quiz.java
+++ WEEK_1_TASK/QuizGenerator/Quiz.java
@@ -1,7 +1,60 @@
 package WEEK_1_TASK.QuizGenerator;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Quiz implements Serializable {
     
+    private String QuizTopicName;  
+    private ArrayList<Question> questionsHolder; //list of questions in a array
+
+   
+    //? Constructor to intialise the quiz 
+    //? Along with set of questions it holds 
+    //? Whenever a instance of "Quiz.java" is called then
+    //? all the above "mentioned" will also be called
+
+    public Quiz(String QuizTopicName){
+        this.QuizTopicName=QuizTopicName;
+        this.questionsHolder=new ArrayList<>();
+    }
+
+    //? adds a question to the quiz
+    //? (each intances of "Question.java" is added to the arraylist<> questions)
+    public void addQuestion(Question question){
+        questionsHolder.add(question);
+    }
+        
+    //? starts the quiz , asks question , 
+    //? and returns score for feedback.
+
+    public void takeQuiz(){
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+        int score = 0;
+
+        System.out.println("\n STARTING QUIZ : " + QuizTopicName);
+        System.out.println("-----------------------");
+
+        for (Question question : questionsHolder){
+                question.displayQuestion(); //shows the pre-created questions
+                System.out.println("Your answer (1-4): ");
+                int userAnswer=scanner.nextInt();
+
+                if (question.isCorrect(userAnswer)) {
+                    System.out.println("Correct ! \n");
+                    score++;
+                }else{
+                    System.out.println("InCorrect ! \n");
+                }
+                
+                System.out.println("Quiz complete ! Your Score : " +score+"/"+questionsHolder.size());
+
+        } 
+    }
+      //shows the name of the quiz at the end
+      public String getName() {
+        return QuizTopicName;
+    }
 }
